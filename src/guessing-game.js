c1cb70e645700a670eb08bb35114d82b78bd2d2d
Darya Milko <milkodaria@gmail.com>
2017-09-28 23:28:44 +0300

    guess too

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,28 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.less=0;
+        this.half=0;
+        this.more=0;
+    }
 
     setRange(min, max) {
-
+        this.less=min;
+        this.more=max;
     }
 
     guess() {
-
+        this.half=Math.round((this.less+this.more)/2); //num.toFixed() ?
+        return this.half; 
     }
 
     lower() {
-
+        this.more=this.half; 
+        return this;
     }
 
     greater() {
-
+        this.less=this.half; 
+        return this;
     }
 }
 
